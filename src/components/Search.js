cf46308356dc3fa22dcf0ed196f6b6777a137bd6
Ryan Alumbaugh <ralumbaugh@gmail.com>
2020-08-20 11:42:43 -0700

    Following tutorial on deploying to github pages

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { navigate } from '@reach/router';
 
 const Search = () =>{
     const [newCategory, setNewCategory] = useState("people");
     const [newID, setNewID] = useState("");
+    const [allPeople, setAllPeople] = useState([]);
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         navigate(`/${newCategory}/${newID}`)
+    }
+// Can use this next line to grab the names of people to later map a dropdown box
+    // useEffect(() =>{
+    //     fetch(`https://swapi.dev/api/people/`)
+    //         .then(response => {
+    //             return response.json();
+    //         })
+    //         .then(response => {
+    //             console.log(response);
+    //             setAllPeople(response.results);
+    //         })
+    // }, [])
+
+    const showAllPeople = () =>{
+        console.log(allPeople);
     }
 
     return(
@@ -19,6 +35,14 @@ const Search = () =>{
                 <option value="species">Species</option>
                 <option value="planets">Planet</option>
             </select>
+            <button onClick={showAllPeople}>Click me</button>
+            {/* <select id="allPeople">
+                {
+                    allPeople.map((person, i) =>
+                    <option value="person">{person.name}</option>
+                    )
+                }
+            </select> */}
             <label htmlFor="ID">ID: </label>
             <input type="number" id="ID" onChange={(e) => setNewID(e.target.value)}></input>
             <button>Search</button>
